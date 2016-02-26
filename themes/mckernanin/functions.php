cfe0be87c5524a5fe45b59f8c6f973ee445bcda3
Kevin McKernan <kevin@mckernan.in>
2016-02-25 19:18:43 -0700

    initial commit

---
+++ themes/mckernanin/functions.php
@@ -0,0 +1,38 @@
+<?php
+function cc_mime_types( $mimes ) {
+	$mimes['svg'] = 'image/svg+xml';
+
+	return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+
+/**
+ * TypeKit Fonts.
+ *
+ * @since Theme 1.0
+ */
+function theme_typekit() {
+	wp_enqueue_script( 'theme_typekit', '//use.typekit.net/wai6zgw.js' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+add_action( 'admin_enqueue_scripts', 'theme_typekit' );
+
+function theme_typekit_inline() {
+	if ( wp_script_is( 'theme_typekit', 'done' ) ) {
+		?>
+	  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php
+	}
+}
+add_action( 'wp_head', 'theme_typekit_inline' );
+add_action( 'admin_head', 'theme_typekit_inline' );
+
+function mckernanin_assets() {
+	wp_deregister_style( 'stag-custom-style' );
+}
+add_action( 'wp_print_styles', 'mckernanin_assets', 100 );
+
+function mck_add_editor_styles() {
+	add_editor_style( get_stylesheet_directory_uri() .'/editor-styles.css' );
+}
+add_action( 'admin_init', 'mck_add_editor_styles' );
